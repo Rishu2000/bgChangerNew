51e50f952f236ab5855903a07d383ddde09fb48e
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-02-16 12:32:05 +0530

    Added "reset" TouchableOpacity.

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import React, {useState} from "react"
 import {Text,StyleSheet,View,TouchableOpacity, Touchable,StatusBar} from "react-native"
 const App = () => {
   const [bgColor,setBgColor] = useState("rgb(32,0,126)")
-  const [name,setName] = useState("Rishav")
   const handleBGC = () => {
     let color = "rgb("+Math.floor(Math.random() * 256)+","+Math.floor(Math.random() * 256)+","+Math.floor(Math.random() * 256)+")";
     setBgColor(color);
+  }
+  const resetBGC = () => {
+    setBgColor("rgb(0,0,0)");
   }
   return(
     <>
@@ -13,6 +15,9 @@ const App = () => {
     <View style={[styles.container,{backgroundColor:bgColor}]}>
     <TouchableOpacity onPress={handleBGC}>
     <Text style={styles.text}>tap me</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={resetBGC}/*{setBgColor("rgb(0,0,0)")}*/>
+    <Text style={[styles.text,{marginTop:20}]}>reset</Text>
     </TouchableOpacity>
     </View>
     </>
